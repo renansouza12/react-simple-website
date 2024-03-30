205f7c8890545af9b48bb10fc45c23c00b0c554a
Renan Souza <renansouza0122@gmail.com>
2024-03-30 13:38:53 -0300

    :sparkles: feat: add hero title animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,12 +1,28 @@
+import { useLayoutEffect } from 'react'
+import {gsap} from 'gsap'
+import SplitType from 'split-type'
 import './Hero.scss'
 
 
 export default function Hero(){
+    useLayoutEffect(()=>{
+
+        const heroTitle = new SplitType('.hero_title_txt');
+        const charHeroTitle = document.querySelectorAll('.hero_title_txt .char');
+
+        gsap.from(charHeroTitle,{
+            x:150,
+            stagger:0.1,
+            opacity:0
+        })
+
+    },[])
+
     return(
         <div className="hero">
             <div className="hero_title">    
 
-                <h1>Chic modern love</h1>
+                <h1 className='hero_title_txt'>Chic modern love</h1>
 
             </div>
            
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,8 @@
 import './Header.scss'
 
 export default function Header() {
+
+
     return (
         <header>
             <nav>
